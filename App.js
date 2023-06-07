c24dac0cda5db2e66121e5e5cbadfb96f7cd1aab
6derick6 <6derick6@gmail.com>
2023-06-07 12:58:07 -0300

    Exercício Lista de Filmes

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [movies,setMovies] = useState(null);
+
+  useEffect(()=>{
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(function(json){
+
+      setMovies(json);
+
+
+
+    })
+  },[])
+
+  if(movies != null){
   return (
     <View style={styles.container}>
-      <Text>teste</Text>
       <StatusBar style="auto" />
+
+      {
+        movies.results.map(function(val){
+          return(
+            <Text>{val.original_title}</Text>
+          )
+        })
+      }
     </View>
   );
+
+  }else{
+    return(
+      <View style={styles.container}>
+        <Text>Carregando...</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
