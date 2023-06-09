589b1f8977b52bc3f6875a77d6dcc2a5b9ffdb16
6derick6 <6derick6@gmail.com>
2023-06-09 12:49:00 -0300

    Estilizando app lista de filmes

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 export default function App() {
 
   const [movies,setMovies] = useState(null);
+  const [abas,setAbas] = useState(0);
 
   useEffect(()=>{
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1', {
@@ -21,34 +22,47 @@ export default function App() {
   },[])
 
   if(movies != null){
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-
-      {
-        movies.results.map(function(val){
-          return(
-            <Text>{val.original_title}</Text>
-          )
-        })
-      }
-    </View>
-  );
-
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden />
+        {
+          movies.results.map(function(val){
+            if(val.id == abas){
+              return(
+                <View>
+                  <TouchableOpacity onPress={()=>setAbas(val.id)}>
+                    <Text style={{color:'white'}}>{val.original_title}</Text>
+                  </TouchableOpacity>
+                  <Text style={{color:'white'}}>{val.overview}</Text>
+                </View>  
+              )
+            }else{
+              return(
+                <View>
+                  <TouchableOpacity onPress={()=>setAbas(val.id)}>
+                    <Text style={{color:'black'}}>{val.original_title}</Text>
+                  </TouchableOpacity>
+                </View>  
+              )
+            }
+          })
+        }
+      </View>
+    )
   }else{
-    return(
+    return (
       <View style={styles.container}>
         <Text>Carregando...</Text>
       </View>
     )
   }
+  
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#069',
   },
 });
