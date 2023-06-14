ebbac3866f09ac916afd96e7f2f9244252c1c773
6derick6 <6derick6@gmail.com>
2023-06-14 13:08:58 -0300

    Atualizando App

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
 
   const [movies,setMovies] = useState(null);
   const [abas,setAbas] = useState(0);
@@ -21,43 +23,45 @@ export default function App() {
     })
   },[])
 
-  if(movies != null){
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden />
-        {
-          movies.results.map(function(val){
-            if(val.id == abas){
-              return(
-                <View>
-                  <TouchableOpacity onPress={()=>setAbas(val.id)}>
-                    <Text style={{color:'white'}}>{val.original_title}</Text>
-                  </TouchableOpacity>
-                  <Text style={{color:'white'}}>{val.overview}</Text>
-                </View>  
-              )
-            }else{
-              return(
-                <View>
-                  <TouchableOpacity onPress={()=>setAbas(val.id)}>
-                    <Text style={{color:'black'}}>{val.original_title}</Text>
-                  </TouchableOpacity>
-                </View>  
-              )
-            }
-          })
-        }
-      </View>
-    )
-  }else{
-    return (
-      <View style={styles.container}>
-        <Text>Carregando...</Text>
-      </View>
-    )
-  }
-  
+  return (
+    <View style={styles.container}>
+          <StatusBar hidden />
+          {
+            movies.results.map(function(val){
+              if(val.id == abas){
+                return(
+                  <View>
+                    <TouchableOpacity onPress={()=>setAbas(val.id)}>
+                      <Text style={{color:'white'}}>{val.original_title}</Text>
+                    </TouchableOpacity>
+                    <Text style={{color:'white'}}>{val.overview}</Text>
+                  </View>  
+                )
+              }else{
+                return(
+                  <View>
+                    <TouchableOpacity onPress={()=>setAbas(val.id)}>
+                      <Text style={{color:'black'}}>{val.original_title}</Text>
+                    </TouchableOpacity>
+                  </View>  
+                )
+              }
+            })
+          }
+        </View>
+  );
+}
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>  
+    )
 }
 
 const styles = StyleSheet.create({
